e36cfff98a8b225f15890cdbfa1f14704234d623
heidechv <heidechv@rose-hulman.edu>
2017-12-06 11:36:26 -0500

    problem2 in progress

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 PRACTICE Test 1, problem 2.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Hailey Heidecker.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -102,7 +102,7 @@ def problem2a(circle, rectangle, window):
       :type window:    rg.RoseWindow
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # ------------------------------------------------------------------
     # ------------------------------------------------------------------
@@ -110,6 +110,22 @@ def problem2a(circle, rectangle, window):
     #    DIFFICULTY:      6
     #    TIME ESTIMATE:   10 to 15 minutes.
     # ------------------------------------------------------------------
+
+    circle.attach_to(window)
+    rectangle.attach_to(window)
+    window.render()
+    window.continue_on_mouse_click()
+
+    line = rg.Line(rectangle.get_upper_right_corner(), rectangle.get_lower_left_corner())
+    line.arrow = 'last'
+    line.attach_to(window)
+    window.render()
+    window.continue_on_mouse_click()
+
+    circle.fill_color = rectangle.outline_color
+
+    window.render()
+
 
 def run_test_problem2b():
     """ Tests the  problem2b   function. """
@@ -181,6 +197,21 @@ def problem2b(rect, n, delta, win):
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:   15 to 25 minutes.
     # ------------------------------------------------------------------
+
+     x1 = rect.corner_1.x
+     y1 = rect.corner_1.y
+     x2 = rect.corner_2.x
+     y2 = rect.corner_2.y
+    for k in range(n + 1):
+        rect.get_width = rect.get_width + (2 * delta)
+        rect.get_height = rect.get_height + (2 * delta)
+
+        x1 = x1 - (rect.get_width/2)
+        y1 = y1 - (rect.get_width / 2)
+        x2 = x2 + (rect.get_width / 2)
+        y2 = y2 + (rect.get_width / 2)
+
+        rectangle = rg.Rectangle(rg.Point(x1, y1), rg.Point(x2, y2))
 
 
 # ----------------------------------------------------------------------
